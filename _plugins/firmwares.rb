1edf68a634f3d50ad1866db788397ec7f96e17e7
Nils Schneider <nils@nilsschneider.net>
2015-01-07 15:13:40 +0100

    firmware: 0.6, add new models. CPEs do not work yet

--- _plugins/firmwares.rb
+++ _plugins/firmwares.rb
@@ -4,33 +4,47 @@ require 'nokogiri'
 require 'pp'
 
 FIRMWARE_REGEX = /^gluon-((\w+)-([\d.]+)-([\w-]+)).bin$/
-FIRMWARE_BASE = "http://luebeck.freifunk.net/firmware/0.5/"
+FIRMWARE_BASE = "http://luebeck.freifunk.net/firmware/0.6/"
 HWREV_REGEX = /^(.+?)(?:-rev)?-(\w\d)$/
 
 MODELMAP = {
-  "ubiquiti-bullet-m"        => { :make => "Ubiquiti", :model => "Bullet M, Nanostation Loco M" },
-  "ubiquiti-unifi"           => { :make => "Ubiquiti", :model => "UniFi AP (LR)" },
-  "ubiquiti-nanostation-m"   => { :make => "Ubiquiti", :model => "Nanostation M" },
-  "ubiquiti-unifiap-outdoor" => { :make => "Ubiquiti", :model => "UniFi AP Outdoor" },
+  "buffalo-wzr-hp-ag300h-wzr-600dhp" => { :make => "Buffalo", :model => "WZR-HP-AG300H/600DHP" },
+  "buffalo-wzr-hp-g450h"             => { :make => "Buffalo", :model => "WZR-HP-G450H" },
 
+  "d-link-dir-615"  => { :make => "D-Link", :model => "D-Link DIR-615" },
+  "d-link-dir-825"  => { :make => "D-Link", :model => "D-Link DIR-825" },
+
+  "linksys-wrt160nl" => { :make => "Linksys", :model => "WRT160NL" },
+
+  "tp-link-cpe210"         => { :make => "TP-Link", :model => "CPE210" },
+  "tp-link-cpe220"         => { :make => "TP-Link", :model => "CPE220" },
+  "tp-link-cpe510"         => { :make => "TP-Link", :model => "CPE510" },
+  "tp-link-cpe520"         => { :make => "TP-Link", :model => "CPE520" },
   "tp-link-tl-mr3020"      => { :make => "TP-Link", :model => "TL-MR3020" },
   "tp-link-tl-mr3040"      => { :make => "TP-Link", :model => "TL-MR3040" },
   "tp-link-tl-mr3220"      => { :make => "TP-Link", :model => "TL-MR3220" },
   "tp-link-tl-mr3420"      => { :make => "TP-Link", :model => "TL-MR3420" },
+  "tp-link-tl-wa750re"     => { :make => "TP-Link", :model => "TL-WA750RE" },
+  "tp-link-tl-wa801n-nd"   => { :make => "TP-Link", :model => "TL-WA801" },
+  "tp-link-tl-wa850re"     => { :make => "TP-Link", :model => "TL-WA850RE" },
+  "tp-link-tl-wa901n-nd"   => { :make => "TP-Link", :model => "TL-WA901" },
   "tp-link-tl-wdr3500"     => { :make => "TP-Link", :model => "TL-WDR3500" },
   "tp-link-tl-wdr3600"     => { :make => "TP-Link", :model => "TL-WDR3600" },
   "tp-link-tl-wdr4300"     => { :make => "TP-Link", :model => "TL-WDR4300" },
-  "tp-link-tl-wa901n-nd"   => { :make => "TP-Link", :model => "TL-WA901" },
+  "tp-link-tl-wr1043n-nd"  => { :make => "TP-Link", :model => "TL-WR1043" },
+  "tp-link-tl-wr703n"      => { :make => "TP-Link", :model => "TL-WR703" },
+  "tp-link-tl-wr710n"      => { :make => "TP-Link", :model => "TL-WR710" },
   "tp-link-tl-wr740n-nd"   => { :make => "TP-Link", :model => "TL-WR740" },
   "tp-link-tl-wr741n-nd"   => { :make => "TP-Link", :model => "TL-WR741" },
   "tp-link-tl-wr841n-nd"   => { :make => "TP-Link", :model => "TL-WR841" },
   "tp-link-tl-wr842n-nd"   => { :make => "TP-Link", :model => "TL-WR842" },
   "tp-link-tl-wr901n-nd"   => { :make => "TP-Link", :model => "TL-WR941" },
   "tp-link-tl-wr941n-nd"   => { :make => "TP-Link", :model => "TL-WR941" },
-  "tp-link-tl-wr1043n-nd"  => { :make => "TP-Link", :model => "TL-WR1043" },
 
-  "d-link-dir-615"  => { :make => "D-Link", :model => "D-Link DIR-615" },
-  "d-link-dir-825"  => { :make => "D-Link", :model => "D-Link DIR-825" },
+  "ubiquiti-bullet-m"        => { :make => "Ubiquiti", :model => "Bullet M, Nanostation Loco M" },
+  "ubiquiti-nanostation-m"   => { :make => "Ubiquiti", :model => "Nanostation M" },
+  "ubiquiti-unifiap-outdoor" => { :make => "Ubiquiti", :model => "UniFi AP Outdoor" },
+  "ubiquiti-unifi"           => { :make => "Ubiquiti", :model => "UniFi AP (LR)" },
 }
 
 module Jekyll
